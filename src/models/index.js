7dd983f6544cc5480fca0b2ed4856e525ba22430
Edumanning <edugonzalez001@gmail.com>
2023-08-17 22:10:01 -0500

    avances2 clase4

--- src/models/index.js
+++ src/models/index.js
@@ -1,13 +1,26 @@
 const Cart = require("./Cart");
 const Category = require("./Category");
 const Product = require("./Product");
+const Purchase = require("./Purchase");
 const User = require("./User");
 
 
 //Product -> catergoryId
-
 Product.belongsTo(Category) 
 Category.hasMany(Product)
 
+//Cart -> userId
 Cart.belongsTo(User)
 User.hasMany(Cart)
+
+//Cart -> productId
+Cart.belongsTo(Product)
+Product.hasMany(Cart)
+
+//Purchase -> userId
+Purchase.belongsTo(User)
+User.hasMany(Cart)
+
+//Purchase -> productId
+Purchase.belongsTo(Product)
+Product.hasMany(Purchase)
